33cbbe460e51eb0bb717ff11a435183bf31d24f6
Ninh Bình <ninhhbinhh@gmail.com>
2023-11-14 14:19:26 +0700

    category

--- resources/views/pages/client/home.php
+++ resources/views/pages/client/home.php
@@ -10,7 +10,90 @@
                 </h2>
             </div>
             <div class="click-slider">
-                
+                <div class="click-next">
+                    <button class="slider-arrow prve-arrow">
+                        <i class="fa-solid fa-arrow-left"></i>
+                    </button>
+                    <button class="slider-arrow next-arrow">
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </button>
+                </div>
+                <div class="click-list">
+                    <div class="click-track">
+                        <div class="click-slide">
+                            <a href="">
+                                <img src="<?php APP_URL ?>public/assets/category/elec-1.webp" alt="">
+                                <h6>Computer</h6>
+                            </a>
+                        </div>
+                        <div class="click-slide">
+                            <a href="">
+                                <img src="<?php APP_URL ?>public/assets/banner-kid.png" alt="">
+                                <h6>kids</h6>
+                            </a>
+                        </div>
+                        <div class="click-slide">
+                            <a href="">
+                                <img src="<?php APP_URL ?>public/assets/category/elec-2.webp" alt="">
+                                <h6>Laptop</h6>
+                            </a>
+                        </div>
+                        <div class="click-slide">
+                            <a href="">
+                                <img src="<?php APP_URL ?>public/assets/category/elec-11.webp" alt="">
+                                <h6>Headphones</h6>
+                            </a>
+                        </div>
+                        <div class="click-slide">
+                            <a href="">
+                                <img src="<?php APP_URL ?>public/assets/category/elec-4.webp" alt="">
+                                <h6>Camera & Photosdfsdf</h6>
+                            </a>
+                        </div>
+                        <div class="click-slide">
+                            <a href="">
+                                <img src="<?php APP_URL ?>public/assets/category/elec-5.webp" alt="">
+                                <h6>kids</h6>
+                            </a>
+                        </div>
+                        <div class="click-slide">
+                            <a href="">
+                                <img src="<?php APP_URL ?>public/assets/category/elec-11.webp" alt="">
+                                <h6>Headphones</h6>
+                            </a>
+                        </div>
+                        <div class="click-slide">
+                            <a href="">
+                                <img src="<?php APP_URL ?>public/assets/category/elec-4.webp" alt="">
+                                <h6>Camera & Photosdfsdf</h6>
+                            </a>
+                        </div>
+                        <div class="click-slide">
+                            <a href="">
+                                <img src="<?php APP_URL ?>public/assets/category/elec-5.webp" alt="">
+                                <h6>kids</h6>
+                            </a>
+                        </div>
+                        <div class="click-slide">
+                            <a href="">
+                                <img src="<?php APP_URL ?>public/assets/category/elec-11.webp" alt="">
+                                <h6>Headphones</h6>
+                            </a>
+                        </div>
+                        <div class="click-slide">
+                            <a href="">
+                                <img src="<?php APP_URL ?>public/assets/category/elec-4.webp" alt="">
+                                <h6>Camera & Photosdfsdf</h6>
+                            </a>
+                        </div>
+                        <div class="click-slide">
+                            <a href="">
+                                <img src="<?php APP_URL ?>public/assets/category/elec-5.webp" alt="">
+                                <h6>kids</h6>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </main>
